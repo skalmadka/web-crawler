21b21fcc120b00d398199d65d6eb86ebe6d0b40d
Sunil Kalmadka <skalmadka@gmail.com>
2015-05-26 03:11:02 -0400

    UI update

--- crawler-web-server/app.js
+++ crawler-web-server/app.js
@@ -10,6 +10,10 @@ var routes = require('./routes/index');
 //Helper Objects
 var users = require('./users');
 
+//Storm DRPC
+var NodeDRPCClient = require('node-drpc');
+var nodeDrpcClient =  new  NodeDRPCClient( process.env.DRPC_SERVER_HOST,  parseInt(process.env.DRPC_SERVER_PORT, 10) );
+
 //Kafka
 var kafka = require('kafka-node'),
     Producer = kafka.Producer,
@@ -18,6 +22,7 @@ var kafka = require('kafka-node'),
 
 var topic_name_crawler = process.env.KAFKA_TOPIC_CRAWL;
 var crawl_depth =  process.env.CRAWL_DEFAULT_DEPTH;
+var topic_name_doc = process.env.KAFKA_TOPIC_DOC;
 
 producer.on('ready', function () {
   console.log('Kafka Producer Ready');
@@ -55,20 +60,16 @@ app.use('/api',function(req, res, next){
   // key is invalid
   if (!~users[userid]) return next(error(401, 'invalid user'));
 
-  // all good, store req.key for route access
+  // all good
   next();
 });
 
 app.post('/api/event', function(req, res, next){
-  console.log('Looking for POST data...');
+  console.log('/api/event: Looking for POST data...');
   //Get event from post data
-  //Event : {type: file/url, uri:fileName/url, summary:<text>}
   var event = new Object();
   event.userid = req.body.userid;
-  event.type = req.body.type;
   event.uri = req.body.uri;
-  event.summary = req.body.summary;
-  event.timestamp = req.body.timestamp;
 
   var payload_crawler = [{topic:topic_name_crawler , messages:event.uri+' '+crawl_depth}];
 
@@ -83,35 +84,75 @@ app.post('/api/event', function(req, res, next){
       }
     });
   } else {
-    res.send(500, 'Producer is not initialized');
+    res.send(500, 'Unable to send to Kafka Producer');
   }
 });
 
+//DRPC Search Route
+app.post('/api/search', function(req, res, next){
+  console.log('/api/search: Looking for POST data...');
 
-//URL Feeder
-app.post('/api/url_feeder', function(req, res, next){
-  console.log('Looking for POST data...');
-  var event = new Object();
-  event.userid = req.body.userid;
-  event.uri = req.body.uri;
-  event.crawl_depth = req.body.crawl_depth;
+  var userid = req.body.userid;
+  var searchQuery = req.body.searchQuery;
 
-  var payload_crawler = [{topic:topic_name_crawler , messages:event.uri+' '+event.crawl_depth}];
+  nodeDrpcClient.execute("search", searchQuery, function(err, response) {//DRPC func_name, func_args, callback
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Storm DRPC Success");
 
-  console.log('Sending to kafka...');
-  //Send to Kafka
-  if(producer) {
-    producer.send(payload_crawler, function (err, data) {
-      if (err) {
-        res.send(500, err);
-      } else {
-        res.send(200, 'Message is queued.');
+      var searchResultArray = [];
+      var jsonResponse = JSON.parse(response);
+      for(var i = 0; i < jsonResponse.length; i++) {
+        var curResponse = jsonResponse[i];
+        var curSearchResult = curResponse[curResponse.length -1];
+        searchResultArray.push(curSearchResult)
+
+        console.log("curResponse"+ i + " : "+JSON.stringify(curResponse));
       }
-    });
-  } else {
-    res.send(500, 'Producer is not initialized');
-  }
+
+      console.log("searchResultArray"+ JSON.stringify(searchResultArray));
+      res.render('search_result', {searchResult: searchResultArray});
+      /*res.render('search_result', {searchResult: searchResultArray}, function(err, html) {
+       res.send(200, JSON.stringify(searchResultArray));
+       })*/
+    }
+  });
 });
+
+//Voice Search
+app.get('/speech_service', function(req, res, next){
+  console.log('/speech_service: Looking for GET data...');
+
+  var user_utterance = req.query.utterance;
+  console.log('/speech_service:'+user_utterance);
+  nodeDrpcClient.execute("search", user_utterance, function(err, response) {//DRPC func_name, func_args, callback
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Storm DRPC Success");
+
+      var searchResultArray = [];
+      var jsonResponse = JSON.parse(response);
+      for(var i = 0; i < jsonResponse.length; i++) {
+        var curResponse = jsonResponse[i];
+        var curSearchResult = curResponse[curResponse.length -1];
+        searchResultArray.push(curSearchResult)
+
+        console.log("curResponse"+ i + " : "+JSON.stringify(curResponse));
+      }
+
+      var count =  (searchResultArray.length == 0)? "no" : searchResultArray.length;
+
+      console.log("searchResultArray"+ JSON.stringify(searchResultArray));
+      res.send(200, {current_response:count, search_results: searchResultArray});
+
+      //res.render('search_result', {searchResult: searchResultArray});
+
+    }
+  });
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- crawler-web-server/routes/index.js
+++ crawler-web-server/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET search page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Search' });
 });
+
+/* GET url_feeder page. */
+router.get('/url_feeder', function(req, res, next) {
+  res.render('url_feeder', { title: 'URL Feeder' });
+});
+
+/* GET voice_search page. */
+router.get('/voice_search', function(req, res, next) {
+  res.render('voice_search', { title: 'Voice Search' });
+});
+
+
+/* GET task_summary page. */
+/*
+router.get('/task_summary', function(req, res, next) {
+  res.render('task_summary', { title: 'Task Summary' });
+});
+*/
 
 module.exports = router;
